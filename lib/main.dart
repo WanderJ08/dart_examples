6ebda7fd5d61850b5a2e85d22d10c9309ca8f100
WanderJ08 <wanderroque01@gmail.com>
2024-10-06 18:14:09 -0400

    chore:New examples added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,39 @@
 var addition = 3 + 2;
 var word = 'Hello';
-var additionString = 3 + 2;
+var additionString = "3 + 2";
+int? a = null;
+
+class MyClass {
+  String? someProperty;
+
+  void someMethod() {
+    print("Method called");
+  }
+}
+
+final aListOfStrings = ['one', 'two', 'three'];
+final aSetOfStrings = {'one', 'two', 'three'};
+final aMapOfStringsToInts = {
+  'one': 1,
+  'two': 2,
+  'three': 3,
+};
 
 void main() {
   print(addition);
   print(word.toUpperCase());
   print(additionString.toString());
+
+  print(a); // Prints "null"
+  print(1 ?? 3); // Prints "1"
+  print(null ?? 12); // Prints "12"
+
+  MyClass? myObject = MyClass();
+  print(myObject?.someProperty); // Prints "null"
+
+  myObject?.someMethod(); // Calls the method if myObject is not null
+
+  print(aListOfStrings);
+  print(aSetOfStrings);
+  print(aMapOfStringsToInts);
 }
